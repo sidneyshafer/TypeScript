bf0313e6c972ae02d41566c0852cb02b41547094
Sidney Shafer <sidney@mnshafers.com>
2025-01-18 06:40:41 -0600

    Add Type Guards

--- Section-6-Advanced-Type-Concepts/06-Project/dist/app.js
+++ Section-6-Advanced-Type-Concepts/06-Project/dist/app.js
@@ -4,3 +4,49 @@ const e1 = {
     privileges: ['create', 'delete'],
     startDate: new Date()
 };
+// Type Guards
+function add(a, b) {
+    // Type Guards
+    if (typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+function printEmployeeInformation(emp) {
+    console.log('Name: ' + emp.name);
+    // More on Type Guards
+    if ('privileges' in emp) {
+        console.log('Privileges: ' + emp.privileges);
+    }
+    if ('startDate' in emp) {
+        console.log('Start Date: ' + emp.startDate);
+    }
+}
+printEmployeeInformation(e1);
+class Car {
+    drive() {
+        console.log('Driving...');
+    }
+}
+class Truck {
+    drive() {
+        console.log('Driving a truck...');
+    }
+    loadCargo(amount) {
+        console.log('Loading cargo...' + amount);
+    }
+}
+const v1 = new Car();
+const v2 = new Truck();
+function useVehicle(vehicle) {
+    vehicle.drive();
+    // if('loadCargo' in vehicle) {
+    //     vehicle.loadCargo(1000);
+    // }
+    // Type Guards - Using instanceOf
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(1000);
+    }
+}
+useVehicle(v1);
+useVehicle(v2);
--- Section-6-Advanced-Type-Concepts/06-Project/src/app.ts
+++ Section-6-Advanced-Type-Concepts/06-Project/src/app.ts
@@ -21,3 +21,62 @@ type Combinable = string | number;
 type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
+
+// Type Guards
+function add(a: Combinable, b: Combinable) {
+    // Type Guards
+    if(typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+    console.log('Name: ' + emp.name);
+    // More on Type Guards
+    if('privileges' in emp) {
+        console.log('Privileges: ' + emp.privileges);
+    }
+    if('startDate' in emp) {
+        console.log('Start Date: ' + emp.startDate);
+    }
+}
+
+printEmployeeInformation(e1);
+
+class Car {
+    drive() {
+        console.log('Driving...');
+    }
+}
+
+class Truck {
+    drive() {
+        console.log('Driving a truck...')
+    }
+
+    loadCargo(amount: number) {
+        console.log('Loading cargo...' + amount);
+    }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+    vehicle.drive();
+    // if('loadCargo' in vehicle) {
+    //     vehicle.loadCargo(1000);
+    // }
+    // Type Guards - Using instanceOf
+    if(vehicle instanceof Truck) {
+        vehicle.loadCargo(1000);
+    }
+}
+
+useVehicle(v1);
+useVehicle(v2);
