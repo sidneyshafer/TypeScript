83496e8ead4bd4aa207e7d634dce30fb4e04536b
Sidney Shafer <sidney@mnshafers.com>
2025-01-20 06:20:04 -0600

    Add Discriminated Unions

--- Section-6-Advanced-Type-Concepts/06-Project/src/app.ts
+++ Section-6-Advanced-Type-Concepts/06-Project/src/app.ts
@@ -80,3 +80,31 @@ function useVehicle(vehicle: Vehicle) {
 
 useVehicle(v1);
 useVehicle(v2);
+
+// Discriminated Unions
+interface Bird {
+    type: 'bird';
+    flyingSpeed: number;
+}
+
+interface Horse {
+    type: 'horse';
+    runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+    let speed;
+    switch(animal.type) {
+        case "bird":
+            speed = animal.flyingSpeed;
+            break;
+        case "horse":
+            speed = animal.runningSpeed;
+            break;
+    }
+    console.log('Moving with speed: ' + speed);
+}
+
+moveAnimal({type: 'bird', flyingSpeed: 20});
