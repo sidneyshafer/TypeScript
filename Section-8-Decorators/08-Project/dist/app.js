89040469000d08abb09f9624f306ca9c0aad6114
Sidney Shafer <sidney@mnshafers.com>
2025-01-22 08:56:24 -0600

    First look at class decorators

--- Section-8-Decorators/08-Project/dist/app.js
+++ Section-8-Decorators/08-Project/dist/app.js
@@ -1,2 +1,23 @@
 "use strict";
-// Code goes here...
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+// Class Decorator
+function Logger(target) {
+    console.log('Logging...');
+    console.log(target);
+}
+let Person = class Person {
+    constructor() {
+        this.name = 'Sidney';
+        console.log('Creating person object...');
+    }
+};
+Person = __decorate([
+    Logger
+], Person);
+const person = new Person();
+console.log(person);
--- Section-8-Decorators/08-Project/src/app.ts
+++ Section-8-Decorators/08-Project/src/app.ts
@@ -1,1 +1,17 @@
-// Code goes here...
+// Class Decorators
+function Logger(target: Function) {
+    console.log('Logging...');
+    console.log(target);
+}
+
+@Logger
+class Person {
+    name = 'Sidney';
+
+    constructor() {
+        console.log('Creating person object...');
+    }
+}
+
+const person = new Person();
+console.log(person);
