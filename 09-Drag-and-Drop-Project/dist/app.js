b8763ba215adaea92097b196036a9a90885a59f6
Sidney Shafer <sidney@mnshafers.com>
2025-02-10 05:56:46 -0600

    Rendering Project Items with a Class

--- 09-Drag-and-Drop-Project/dist/app.js
+++ 09-Drag-and-Drop-Project/dist/app.js
@@ -116,6 +116,21 @@ class Component {
         this.hostEl.insertAdjacentElement(insertAtStart ? 'afterbegin' : 'beforeend', this.element);
     }
 }
+// ProjectItem Class
+class ProjectItem extends Component {
+    constructor(hostId, project) {
+        super('single-project', hostId, false, project.id);
+        this.project = project;
+        this.configure();
+        this.renderContent();
+    }
+    configure() { }
+    renderContent() {
+        this.element.querySelector('h2').textContent = this.project.title;
+        this.element.querySelector('h3').textContent = this.project.people.toString();
+        this.element.querySelector('p').textContent = this.project.description;
+    }
+}
 // ProjectList Class
 class ProjectList extends Component {
     constructor(type) {
@@ -129,9 +144,7 @@ class ProjectList extends Component {
         const listEl = document.getElementById(`${this.type}-project-list`);
         listEl.innerHTML = '';
         for (const item of this.assignedProjects) {
-            const listItem = document.createElement('li');
-            listItem.textContent = item.title;
-            listEl.appendChild(listItem);
+            new ProjectItem(this.element.querySelector('ul').id, item);
         }
     }
     configure() {
