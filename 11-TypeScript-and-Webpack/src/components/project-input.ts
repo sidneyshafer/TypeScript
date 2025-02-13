014170a416b378e074246033c7af413aa717a98e
Sidney Shafer <sidney@mnshafers.com>
2025-02-13 06:17:28 -0600

    Add Project 11 - TypeScript and Webpack

---
+++ 11-TypeScript-and-Webpack/src/components/project-input.ts
@@ -0,0 +1,98 @@
+import { Component } from "./base-component";
+import { Validate, validate } from "../util/validation";
+import { projectState } from "../state/project-state";
+
+// ProjectInput Class
+export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
+    titleInputEl: HTMLInputElement;
+    descInputEl: HTMLInputElement;
+    peopleInputEl: HTMLInputElement;
+
+    titleErrorEl: HTMLElement;
+    descErrorEl: HTMLElement;
+    peopleErrorEl: HTMLElement;
+
+    constructor() {
+        super('project-input', 'app', true, 'user-input');
+
+        this.titleInputEl = this.element.querySelector('#title') as HTMLInputElement;
+        this.descInputEl = this.element.querySelector('#description') as HTMLInputElement;
+        this.peopleInputEl = this.element.querySelector('#people') as HTMLInputElement;
+
+        this.titleErrorEl = this.element.querySelector('#title-error') as HTMLElement;
+        this.descErrorEl = this.element.querySelector('#description-error') as HTMLElement;
+        this.peopleErrorEl = this.element.querySelector('#people-error') as HTMLElement;
+
+        this.configure();
+    }
+
+    private gatherUserInput(): [string, string, number] | void {
+        const enteredTitle = this.titleInputEl.value;
+        const enteredDesc = this.descInputEl.value;
+        const enteredPeople = this.peopleInputEl.value;
+
+        const titleValidate: Validate = {
+            value: enteredTitle,
+            required: true
+        };
+        const descValidate: Validate = {
+            value: enteredDesc,
+            required: true,
+            minLength: 5
+        };
+        const peopleValidate: Validate = {
+            value: +enteredPeople,
+            required: true,
+            min: 0,
+            max: 5
+        };
+
+        const [isTitleValid, titleError] = validate(titleValidate);
+        const [isDescValid, descError] = validate(descValidate);
+        const [isPeopleValid, peopleError] = validate(peopleValidate);
+
+        // Checking if no input is empty
+        if ( 
+            !isTitleValid || 
+            !isDescValid || 
+            !isPeopleValid
+        ) {
+            this.titleErrorEl.innerText = titleError;
+            this.descErrorEl.innerText = descError;
+            this.peopleErrorEl.innerText = peopleError;
+            return;
+        } else {
+            return [enteredTitle, enteredDesc, +enteredPeople];
+        }
+    }
+
+    configure(): void {
+        this.element.addEventListener('submit', this.submitHandler.bind(this));
+    }
+
+    renderContent(): void {}
+
+    private clearInputs() {
+        this.titleInputEl.value = '';
+        this.descInputEl.value = '';
+        this.peopleInputEl.value = '';
+    }
+
+    private clearErrors() {
+        this.titleErrorEl.innerText = '';
+        this.descErrorEl.innerText = '';
+        this.peopleErrorEl.innerText = '';
+    }
+
+    private submitHandler(event: Event) {
+        event.preventDefault();
+        const userInput = this.gatherUserInput();
+        if (Array.isArray(userInput)) {
+            const [title, desc, people] = userInput;
+            
+            projectState.addProject(title, desc, people);
+            this.clearInputs();
+            this.clearErrors();
+        }
+    }
+}
---
+++ 11-TypeScript-and-Webpack/src/components/project-list.ts
@@ -0,0 +1,68 @@
+import { Component } from "./base-component";
+import { ProjectItem } from "./project-item";
+import { Project, ProjectStatus } from "../models/project";
+import { DragTarget } from "../interfaces/drag-drop";
+import { projectState } from "../state/project-state";
+
+export class ProjectList extends Component<HTMLDivElement, HTMLElement> implements DragTarget {
+
+    private assignedProjects: Project[];
+
+    constructor(private type: 'active' | 'finished') {
+        super('project-list', 'app', false, `${type}-projects`);
+        this.assignedProjects = [];
+
+        this.configure();
+        this.renderContent();
+    }
+
+    private renderProjects() {
+        const listEl = document.getElementById(`${this.type}-project-list`) as HTMLUListElement;
+        listEl.innerHTML = '';
+        for (const item of this.assignedProjects) {
+            new ProjectItem(this.element.querySelector('ul')!.id, item);
+        }
+    }
+
+    dragOverHandler(event: DragEvent): void {
+        if (event.dataTransfer && event.dataTransfer.types[0] === 'text/plain') {
+            event.preventDefault();
+            const listEl = this.element.querySelector('ul')!;
+            listEl.classList.add('droppable');
+        }
+    }
+
+    dropHandler(event: DragEvent): void {
+        const prjId = event.dataTransfer!.getData('text/plain');
+        const prjStatus = this.type === 'active' ? ProjectStatus.Active : ProjectStatus.Finished; 
+        projectState.moveProject(prjId, prjStatus);
+    }
+
+    dragLeaveHandler(_: DragEvent): void {
+        const listEl = this.element.querySelector('ul')!;
+        listEl.classList.remove('droppable');
+    }
+
+    configure(): void {
+        this.element.addEventListener('dragover', this.dragOverHandler.bind(this));
+        this.element.addEventListener('dragleave', this.dragLeaveHandler.bind(this));
+        this.element.addEventListener('drop', this.dropHandler.bind(this));
+        
+        projectState.addListener((projects: Project[]) => {
+            const relevantProjects = projects.filter(prj => {
+                if (this.type === 'active') {
+                    return prj.status === ProjectStatus.Active;
+                }
+                return prj.status == ProjectStatus.Finished;
+            });
+            this.assignedProjects = relevantProjects;
+            this.renderProjects();
+        });
+    }
+
+    renderContent() {
+        const listId = `${this.type}-project-list`;
+        this.element.querySelector('ul')!.id = listId;
+        this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + ' PROJECTS';
+    }
+}
